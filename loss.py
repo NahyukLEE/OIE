c15560f9789de3c7df688f1db143e8c07d20aabe
NahyukLEE <nahyuk0113@gmail.com>
2022-10-13 02:09:13 +0900

    add loss functions

--- loss.py
+++ loss.py
@@ -1,4 +1,3 @@
-from tkinter import W
 import numpy as np
 import torch.nn as nn
 
@@ -14,9 +13,17 @@ def recon_loss(mask, gt_i, pred_a, pred_s):
     return loss
 
 def gradient_loss(mask, gt, output):
-    return
+
+    gt = np.multiply(mask, np.log(gt)) # hadamard product
+    pred = np.multiply(mask, np.log(output))
+
+    l1_loss = nn.L1Loss(reduction = 'sum')
+    loss = l1_loss(np.gradient(gt), np.gradient(pred))
+
+    return loss
 
 def scale_invariant_loss(mask, gt, output):
+
     gt = np.multiply(mask, np.log(gt)) # hadamard product
     pred = np.multiply(mask, np.log(output))
 
@@ -26,8 +33,10 @@ def scale_invariant_loss(mask, gt, output):
     return loss
 
 def intrinsic_loss(mask, pred_a, pred_s, gt_a, gt_s, gt_i):
-    albedo_loss = gradient_loss(mask, gt_a, pred_a) + scale_invariant_loss(mask, gt_a, pred_a)
-    shading_loss = gradient_loss(mask, gt_s, pred_s) + scale_invariant_loss(mask, gt_s, pred_s)
+
+    alpha = 0.5
+    albedo_loss = alpha * gradient_loss(mask, gt_a, pred_a) + (1 - alpha) * scale_invariant_loss(mask, gt_a, pred_a)
+    shading_loss = alpha * gradient_loss(mask, gt_s, pred_s) + (1 - alpha) * scale_invariant_loss(mask, gt_s, pred_s)
     reconstruction_loss = recon_loss(mask, gt_i, pred_a, pred_s)
     
     w1 = 0.3 # weight for albedo loss
